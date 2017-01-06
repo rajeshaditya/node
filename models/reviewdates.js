6aedaf4a30b782f14d92c6f1f6c59a9109d57787
kd33pk <bhagatd800@gmail.com>
2017-01-06 14:57:58 +0530

    added vc date backend

---
+++ models/reviewdates.js
@@ -0,0 +1,20 @@
+var monk=require('monk');
+var database = require('../db');
+var db=monk(database.db);
+var reviewdate =db.get('reviewdate');
+
+module.exports.setReviewDate=function(vc_date,principal_date,hod_date,faculty_date,callback){
+	reviewdate.update({ edate: "t" },
+   {
+   	edate: "t",
+      vcdate: vc_date,
+      principaldate: principal_date,
+      hoddate: hod_date,
+      facultydate:faculty_date
+   },
+   { upsert: true },
+   function(err){
+  	 if(err)
+   	return err;
+   });
+}
